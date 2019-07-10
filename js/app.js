8a071b604bd089fdf5327e8e7e7c103ed711c519
geraldogonzalez <geraldodeveloper09@gmail.com>
2019-07-10 13:14:16 -0400

    Correcciones dadas por el diseniador Mario Garranchan

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 /* Cambiar color de fondo con scroll */
 "use strict";
-const headerTag = document.querySelector('.header');
-const bodyTag = document.querySelector('body');
+/*const headerTag = document.querySelector('.header');
+const bodyTag = document.querySelector('body');*/
 const toTop = document.querySelector('.btn-top');
 
-let scrolled = () => {
+/*let scrolled = () => {
     let dec = scrollY / (bodyTag.scrollHeight - innerHeight);
     return Math.floor(dec * 1100);
-}
+}*/
 
 addEventListener('scroll', () => {
-    bodyTag.style.setProperty('background', scrolled() > 50 ? "#232b30" : "#ffacb2");
-    headerTag.style.setProperty('background', scrolled() > 50 ? "#232b30" : "#ffacb2");
+   /* bodyTag.style.setProperty('background', scrolled() > 50 ? "#232b30" : "#ffacb2");
+    headerTag.style.setProperty('background', scrolled() > 50 ? "#232b30" : "#ffacb2");*/
 
     if(window.pageYOffset > 1000) {
         toTop.classList.add("active");
